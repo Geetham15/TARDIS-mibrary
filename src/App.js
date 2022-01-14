2ff00749854412347a5b739607bbcca9b5b92824
Clayton Balzer <clayton.balzer.dev@gmail.com>
2022-01-14 12:39:36 -0700

    small text changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 import Layout from "./components/Layout";
-=======
-import AddBooks from "./components/AddBooks";
-
->>>>>>> 738a187aed06ffe73bfc6410f90ce1cc3ecf1d8d
 function App() {
   const [data, loadData] = useState();
   const fetchData = async () => {
@@ -16,17 +11,10 @@ function App() {
   }, []);
   return (
     <div className="App">
-<<<<<<< HEAD
-      <p>{testMessage}</p>
+      <p>testMessage</p>
       <Layout>
         
       </Layout>
-=======
-      <h1 className="text-3xl font-bold  text-green-700  text-center ">
-        Book Exchange App
-      </h1>
-      <AddBooks />
->>>>>>> 738a187aed06ffe73bfc6410f90ce1cc3ecf1d8d
     </div>
   );
 }

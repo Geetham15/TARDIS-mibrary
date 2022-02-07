32a36accf261caabcc958c2925ddd7a09141aeee
Koltan Huget <koltanhuget@hotmail.com>
2022-02-07 07:06:46 -0700

    fixed signup issue and cleaned up UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,23 @@ import Title from "../components/Title";
 import Map from "../components/Map";
 import Search from "../components/Search.js";
 import AuthenticationContext from "../AuthenticationContext";
+import Login from "./Login.js";
 
 function Home({ bookData, setBookData }) {
   const authContext = useContext(AuthenticationContext);
   return (
-    <div className="container">
-      <Title name="MiBrary" />
-      <Search bookData={bookData} setBookData={setBookData} />
-      {authContext.username && <Map bookData={bookData} />}
+    <div>
+      {authContext.username ? (
+        <div className="container">
+          <Search bookData={bookData} setBookData={setBookData} />
+          <Map bookData={bookData} />
+        </div>
+      ) : (
+        <>
+          <Title name="MiBrary" />
+          <Login />
+        </>
+      )}
       {/* <CardList /> */}
     </div>
   );

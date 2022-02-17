64baddfd3b0ec53b06e70b944c7641c3b2ac7c41
Koltan Huget <koltanhuget@hotmail.com>
2022-02-17 14:12:01 -0700

    fixed chat and added books lent to dashboard

--- src/components/Dashboard/DataTable.js
+++ src/components/Dashboard/DataTable.js
@@ -1,40 +1,6 @@
-import { useContext } from "react";
 import MUIDataTable from "mui-datatables";
-import AuthenticationContext from "../../AuthenticationContext";
 
-function DataTable({ title, books, columns, setBooks }) {
-  const authContext = useContext(AuthenticationContext);
-  async function deleteBook(id) {
-    console.log(id);
-    setBooks(() => {
-      let newBooks = books.filter((book) => {
-        return book.id !== id;
-      });
-      return newBooks;
-    });
-    let response = await fetch("/api/deleteBook", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id, userId: authContext.userId }),
-    });
-    response = await response.json();
-    if (response) {
-      alert("Delete was successful");
-    } else {
-      alert("something went wrong");
-    }
-  }
-  const options = {
-    filterType: "checkbox",
-    serverSide: false,
-    sort: true,
-    onRowsDelete: (rowsDeleted) => {
-      console.log(rowsDeleted.data);
-      for (let i = 0; i < rowsDeleted.data.length; i++) {
-        deleteBook(books[rowsDeleted.data[i].dataIndex].id);
-      }
-    },
-  };
+function DataTable({ title, books, columns, options }) {
   return (
     <div>
       <MUIDataTable
